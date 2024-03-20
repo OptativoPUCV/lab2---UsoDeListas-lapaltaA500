6c578d1ae00921cb1dc76f8368aa4d89445a91df
Your Name <you@example.com>
2024-03-20 20:43:50 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -45,11 +45,9 @@ List* crea_lista()
 {
   List* L = create_list();
   for(int i = 1 ; i <= 10 ; i++)
-    {
       int* elemento = (int *)malloc(sizeof(int));
       *elemento = i;
       pushBack(L, elemento);
-    }
   return L;
 }
 
