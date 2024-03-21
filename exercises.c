f5de8e9bbf6d2cdf08c9f2f836c7e4f33de6de23
Your Name <you@example.com>
2024-03-21 21:52:04 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -123,7 +123,9 @@ paraéntesis balanceados. Retorna 1 si están balanceados,
 0 en caso contrario.
 */
 
-int parentesisBalanceados(char *cadena) {
+int parentesisBalanceados(char *cadena) 
+{
+  
    return 0;
 }
 
